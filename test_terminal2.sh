f37f4a68e4fe1ce7ac9040e052ee2c7bc6d747e5
Jumkot <jumkot@mail.ru>
2024-03-31 19:40:56 +0700

    Создан test_terminal2.sh

---
+++ test_terminal2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+
+echo -e "\E(0"
+
+echo -e "\E[10;5Hlqqqqqqqqqqqqk"
+echo -e "\E[11;5Hx    aaaaaaa x"
+echo -e "\E[12;5Hx    aa   aa x"
+echo -e "\E[13;5Hx    aa   aa x"
+echo -e "\E[14;5Hx         aa x"
+echo -e "\E[15;5Hx         aa x"
+echo -e "\E[16;5Hx         aa x"
+echo -e "\E[17;5Hmqqqqqqqqqqqqj"
+
+echo -e "\E(B"
+
+echo -e "\E[m"
