1ed1fe83ab858464d9c4c7b9f38b9907ce314120
Jumkot <jumkot@mail.ru>
2024-04-12 01:34:20 +0700

    Правка форматирования #9

--- include/myReadKey.h
+++ include/myReadKey.h
@@ -9,27 +9,28 @@ extern struct termios actual, saved;
 
 extern char key_value[16];
 
-enum keys {
-    NONE,
-	F5,
-	F6,
-	UP,
-	DOWN,
-    RIGHT,
-	LEFT,
-	LOAD,
-    SAVE,
-    RESET,
-    RUN,
-    STEP,
-    EDIT,
-    EXIT
+enum keys
+{
+  NONE,
+  F5,
+  F6,
+  UP,
+  DOWN,
+  RIGHT,
+  LEFT,
+  LOAD,
+  SAVE,
+  RESET,
+  RUN,
+  STEP,
+  EDIT,
+  EXIT
 };
 
 int rk_mytermsave ();
 int rk_mytermrestore ();
 int rk_mytermregime (int regime, int vtime, int vmin, int echo, int sigint);
-int rk_readkey(enum keys *key);
+int rk_readkey (enum keys *key);
 int rk_readvalue (int *value, int timeout);
 
 #endif
